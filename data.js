92649ea6260207f9beae906234fde7c7da12fe45
James Eagle <jimeagle@gmail.com>
2026-05-25 09:18:58 +0100

    Update nodes

--- data.js
+++ data.js
@@ -9,24 +9,34 @@ const stardewTasks = [
   { id: "copper_ore", parentId: "mines_entry", label: "Mine Copper Ore" },
   { id: "furnace", parentId: "copper_ore", label: "Build Furnace" },
   { id: "copper_bar", parentId: "furnace", label: "Smelt Copper Bar" },
+  
   { id: "copper_axe", parentId: "copper_bar", label: "Upgrade to Copper Axe" },
+  { id: "large_stumps", parentId: "copper_axe", label: "Chop Large Stumps (Hardwood)" },
+  
   { id: "copper_pickaxe", parentId: "copper_bar", label: "Upgrade to Copper Pickaxe" },
-
-  { id: "mines_20", parentId: "mines_entry", label: "Reach Mines Level 20" },
+  { id: "mines_20", parentId: "copper_pickaxe", label: "Reach Mines Level 20" },
   { id: "ghost_fish", parentId: "mines_20", label: "Catch Ghostfish" },
 
   { id: "mines_40", parentId: "mines_20", label: "Reach Mines Level 40" },
   { id: "iron_ore", parentId: "mines_40", label: "Mine Iron Ore" },
   { id: "iron_bar", parentId: "iron_ore", label: "Smelt Iron Bar" },
+  
   { id: "iron_axe", parentId: "iron_bar", label: "Upgrade to Steel Axe" },
+  { id: "secret_woods", parentId: "iron_axe", label: "Chop Large Logs (Secret Woods)" },
+  { id: "woodskip", parentId: "secret_woods", label: "Catch Woodskip" },
+  { id: "hardwood", parentId: "secret_woods", label: "Chop Hardwood" },
+
   { id: "iron_pickaxe", parentId: "iron_bar", label: "Upgrade to Steel Pickaxe" },
+  { id: "dwarf_access", parentId: "iron_pickaxe", label: "Break Boulder to Dwarf (Mines)" },
   { id: "quality_sprinkler", parentId: "iron_bar", label: "Craft Quality Sprinkler" },
 
   { id: "mines_80", parentId: "mines_40", label: "Reach Mines Level 80" },
   { id: "gold_ore", parentId: "mines_80", label: "Mine Gold Ore" },
   { id: "gold_bar", parentId: "gold_ore", label: "Smelt Gold Bar" },
+  
   { id: "gold_axe", parentId: "gold_bar", label: "Upgrade to Gold Axe" },
   { id: "gold_pickaxe", parentId: "gold_bar", label: "Upgrade to Gold Pickaxe" },
+  { id: "meteorites", parentId: "gold_pickaxe", label: "Break Meteorites" },
 
   { id: "mines_120", parentId: "mines_80", label: "Reach Mines Level 120" },
   { id: "skull_key", parentId: "mines_120", label: "Obtain Skull Key" },
@@ -59,6 +69,12 @@ const stardewTasks = [
 
   { id: "dwarf_scrolls", parentId: "mines_entry", label: "Collect All 4 Dwarf Scrolls" },
   { id: "dwarvish_guide", parentId: "dwarf_scrolls", label: "Get Dwarvish Translation Guide" },
+  { id: "talk_to_dwarf", parentId: "dwarvish_guide", label: "Talk to the Dwarf" },
+
+  // --- ARTIFACTS & SPECIAL CROPS ---
+  { id: "ancient_seed_artifact", parentId: "inherit_farm", label: "Find Ancient Seed Artifact" },
+  { id: "ancient_seed_recipe", parentId: "ancient_seed_artifact", label: "Get Ancient Seed Crafting Recipe" },
+  { id: "ancient_fruit", parentId: "ancient_seed_recipe", label: "Grow Ancient Fruit" },
 
   // --- COMMUNITY CENTER ---
   { id: "cc_unlock", parentId: "inherit_farm", label: "Unlock Community Center" },
@@ -75,6 +91,7 @@ const stardewTasks = [
 
   { id: "fish_tank", parentId: "wizard_potion", label: "Complete Fish Tank" },
   { id: "panning", parentId: "fish_tank", label: "Unlock Copper Pan" },
+  { id: "iridium_pan", parentId: "panning", label: "Upgrade to Iridium Pan" },
 
   { id: "vault", parentId: "wizard_potion", label: "Complete Vault" },
   { id: "bus_repair", parentId: "vault", label: "Repair the Bus / Desert" },
@@ -84,6 +101,9 @@ const stardewTasks = [
   { id: "iridium_bar", parentId: "iridium_ore", label: "Smelt Iridium Bar" },
   { id: "iridium_axe", parentId: "iridium_bar", label: "Upgrade to Iridium Axe" },
   { id: "iridium_pickaxe", parentId: "iridium_bar", label: "Upgrade to Iridium Pickaxe" },
+
+  { id: "prismatic_shard", parentId: "skull_cavern", label: "Find a Prismatic Shard" },
+  { id: "galaxy_sword", parentId: "prismatic_shard", label: "Obtain Galaxy Sword (Desert Ritual)" },
 
   { id: "bulletin_board", parentId: "wizard_potion", label: "Complete Bulletin Board" },
   
@@ -95,27 +115,29 @@ const stardewTasks = [
   { id: "boat_repair", parentId: "willy_invite", label: "Repair Willy's Boat" },
   { id: "ginger_island", parentId: "boat_repair", label: "Visit Ginger Island" },
   { id: "volcano_dungeon", parentId: "ginger_island", label: "Reach Volcano Summit" },
-  { id: "island_farm", parentId: "ginger_island", label: "Unlock Island Farm" },
+  { id: "volcano_forge", parentId: "volcano_dungeon", label: "Unlock Volcano Forge" },
+  { id: "infinity_blade", parentId: "volcano_forge", label: "Forge Infinity Blade" },
+
+  { id: "island_north", parentId: "ginger_island", label: "Unlock Island North (Volcano)" },
+  { id: "island_west", parentId: "ginger_island", label: "Unlock Island West (Farm)" },
+  { id: "island_house", parentId: "island_west", label: "Repair Island Farmhouse" },
+  { id: "island_mailbox", parentId: "island_house", label: "Add Island Mailbox" },
+  { id: "island_obelisk", parentId: "island_house", label: "Build Farm Obelisk" },
+  
   { id: "island_resort", parentId: "ginger_island", label: "Unlock Island Resort" },
-  { id: "qi_walnut_room", parentId: "ginger_island", label: "Enter Mr. Qi's Walnut Room" },
+  { id: "qi_walnut_room", parentId: "ginger_island", label: "Enter Mr. Qi's Walnut Room (100 Walnuts)" },
   { id: "perfection", parentId: "qi_walnut_room", label: "Achieve True Perfection" },
 
   // --- FARM BUILDINGS & PROGRESSION ---
   { id: "silo", parentId: "copper_bar", label: "Build Silo" },
-  { id: "coop", parentId: "wood", label: "Build Coop" },
+  { id: "coop", parentId: "inherit_farm", label: "Build Coop" },
   { id: "big_coop", parentId: "coop", label: "Upgrade to Big Coop" },
   { id: "deluxe_coop", parentId: "big_coop", label: "Upgrade to Deluxe Coop" },
   
-  { id: "barn", parentId: "stone", label: "Build Barn" },
+  { id: "barn", parentId: "inherit_farm", label: "Build Barn" },
   { id: "big_barn", parentId: "barn", label: "Upgrade to Big Barn" },
   { id: "deluxe_barn", parentId: "big_barn", label: "Upgrade to Deluxe Barn" },
 
-  { id: "wood", parentId: "pickaxe", label: "Chop Trees" },
-  { id: "stone", parentId: "pickaxe", label: "Break Rocks" },
-  
-  { id: "secret_woods", parentId: "iron_axe", label: "Enter Secret Woods" },
-  { id: "woodskip", parentId: "secret_woods", label: "Catch Woodskip" },
-  { id: "hardwood", parentId: "secret_woods", label: "Chop Hardwood" },
   { id: "stable", parentId: "hardwood", label: "Build Stable" },
   { id: "horse", parentId: "stable", label: "Get Horse" },
 
@@ -125,6 +147,11 @@ const stardewTasks = [
   { id: "obelisks", parentId: "goblin_problem", label: "Unlock Wizard Buildings" },
   { id: "junimo_huts", parentId: "obelisks", label: "Build Junimo Huts" },
   { id: "gold_clock", parentId: "obelisks", label: "Build Gold Clock" },
+
+  // --- SECRET NOTES ---
+  { id: "winter_mystery", parentId: "inherit_farm", label: "Trigger Winter Mystery Quest" },
+  { id: "magnifying_glass", parentId: "winter_mystery", label: "Obtain Magnifying Glass" },
+  { id: "secret_notes", parentId: "magnifying_glass", label: "Find & Read Secret Notes" },
 
   // --- FARMING & CRAFTING ---
   { id: "parsnip", parentId: "inherit_farm", label: "Grow Parsnips" },
